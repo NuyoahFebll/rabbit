3c431a60531c5ac6eb831b95cd9276ea8564bf53
wsb <3067646066@qq.com>
2023-06-17 22:42:11 +0800

    home-轮播图实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,8 +1,14 @@
 import httpInstance from '@/utils/http';
 
-export function getBannerAPI() {
+//获取banner
+export function getBannerAPI(params = {}) {
+	//默认为1 商品为2
+	const { distributionSite = '1' } = params;
 	return httpInstance({
 		url: 'home/banner',
+		params: {
+			distributionSite,
+		},
 	});
 }
 
