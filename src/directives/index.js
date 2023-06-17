909da22fc643d2d2a7d7b3a2c112aeba962ccc6f
wsb <3067646066@qq.com>
2023-06-17 22:25:47 +0800

    一级分类-面包屑导航渲染

--- src/directives/index.js
+++ src/directives/index.js
@@ -8,7 +8,7 @@ export const lazyPlugin = {
 			mounted(el, binding) {
 				//el:指令绑定的那个元素 img
 				//binding:binding.value 指令等于号后面绑定的表达式的值 图片url
-				console.log(el, binding.value);
+				// console.log(el, binding.value);
 				useIntersectionObserver(el, ([{ isIntersecting }]) => {
 					//isIntersecting 布尔值 检测是否进入视口区域
 					// console.log(isIntersecting);
