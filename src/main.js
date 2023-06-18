4c8d06cfc502340652f17209da0532f657b1a364
wsb <3067646066@qq.com>
2023-06-18 18:15:00 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import '@/styles/common.scss';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 import router from './router';
-const app = createApp(App);
 
 //引入懒加载插件指令并且注册
 import { lazyPlugin } from '@/directives/index';
@@ -14,11 +13,14 @@ import { lazyPlugin } from '@/directives/index';
 // 引入全局组件插件
 import { componentPlugin } from '@/components';
 
+const app = createApp(App);
+const pinia = createPinia();
+
+//注册持久化插件
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
 app.use(componentPlugin);
-
 app.use(lazyPlugin);
-
-app.use(createPinia());
 app.use(router);
-
 app.mount('#app');
